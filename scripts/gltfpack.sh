a8e26f2620dfca68141a7a3d64ec6fd92d47b09d
gbz <gbz@gbzs-MacBook-Pro.local>
2025-12-30 14:06:46 -1000

    new

--- scripts/gltfpack.sh
+++ scripts/gltfpack.sh
@@ -5,5 +5,14 @@ for file in ./animation/*.glb; do
 	
 	echo $base
 	
-	gltfpack -i $file -o ./animation_gltfpack/$name -c -tc -ac
+	gltfpack -i $file -o ./animation_gltfpack/$name -cc -tc -ac
+done
+
+for file in ./weapon/*.glb; do
+	name=${file##*/}
+	base=${name%.glb}
+	
+	echo $base
+	
+	gltfpack -i $file -o ./weapon_gltfpack/$name -cc -tc -ac -ke -kn -vpf
 done
